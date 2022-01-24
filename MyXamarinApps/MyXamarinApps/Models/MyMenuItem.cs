60aa046fd844f0532de0b8374c3b320f24853e0a
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-01-24 15:41:04 +0700

    menambahkan menuitem class

---
+++ MyXamarinApps/MyXamarinApps/Models/MyMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyXamarinApps.Models
+{
+    public class MyMenuItem
+    {
+        public MyMenuItem()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Type TargetType { get; set; }
+        public string ImageIcon { get; set; }
+    }
+}
